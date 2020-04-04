4492998cb87af46af687027022440be2df7a4322
Connie Tran <connietrandev@gmail.com>
2020-04-04 11:04:38 -0400

    Get input for city search

--- Assets/search.js
+++ Assets/search.js
@@ -4,18 +4,24 @@ console.log("Hello Weather!");
 // This is our API key
 var APIKey = "4a56f566a02550ae1a4ca20559e1de75";
 
-console.log("https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=4a56f566a02550ae1a4ca20559e1de75");
 
-$('#city-search').submit(function(event) {
+$(document).ready(function () {
+
+    $('#city-search').submit(currentWeather)
+
+});
+
+
+function currentWeather(event) {
 
     event.preventDefault();
 
-    var searchedCity = $('#city-search').val();
+    var searchedCity = $('#city-text').val();
 
     // Here we are building the URL we need to query the database
     // Sample URL: https://api.openweathermap.org/data/2.5/weather?appid=4a56f566a02550ae1a4ca20559e1de75&q=Atlanta;
     // Sample URL: https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=4a56f566a02550ae1a4ca20559e1de75;
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + "&q=" + searchedCity;
 
     console.log(queryURL);
 
@@ -26,36 +32,26 @@ $('#city-search').submit(function(event) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
+    }).then(function (response) {
 
-            // Log the queryURL
-            console.log(queryURL);
+        // Log the resulting object
+        console.log(response);
 
-            // Log the resulting object
-            console.log(response);
+        // Transfer content to HTML
+        $(".city").text(response.name);
 
-            // Transfer content to HTML
-            $(".city").text(response.name);
+        // Convert the temp to fahrenheit
+        var tempF = Math.round((response.main.temp - 273.15) * 1.80 + 32);
+        // add temp content to html
+        $(".temp").text(tempF);
 
-            // Convert the temp to fahrenheit
-            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-            // add temp content to html
-            $(".temp").text("Temperature (K) " + response.main.temp);
-            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-            $(".wind").text("Wind Speed: " + response.wind.speed);
-            $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".wind").text(response.wind.speed);
+        $(".humidity").text(response.main.humidity);
 
 
-
-            // Log the data in the console as well
-            console.log("Wind Speed: " + response.wind.speed);
-            console.log("Humidity: " + response.main.humidity);
-            console.log("Temperature (F): " + tempF);
-        });
-
-
-});
+        // Log the data in the console as well
+        console.log("Wind Speed: " + response.wind.speed);
+        console.log("Humidity: " + response.main.humidity);
+        console.log("Temperature (F): " + tempF);
+    });
+};
